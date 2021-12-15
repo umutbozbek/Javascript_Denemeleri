819c6e06e3e1182adbaa61e974350d0f459a98ab
umutbozbek <umutbozbek58@gmail.com>
2021-12-15 14:32:39 +0300

    Local_Storage

---
+++ Local_Storage/script.js
@@ -0,0 +1,36 @@
+
+//LOCAL STORAGE
+// let val ;
+//set item
+// const firstName =localStorage.setItem('firstName','Umut');
+// const lastName =localStorage.setItem('lastName','Bozbek');
+// let hobbies=['sinema','araba','gezmek'];
+//get item
+
+// val=localStorage.getItem('firstName');
+// val=localStorage.getItem('lastName');
+//remove item
+// localStorage.removeItem('firstName');
+// val=localStorage.removeItem('lastName');
+
+//clear
+// localStorage.clear();
+
+//set array to storage
+
+// localStorage.setItem('hobbies',JSON.stringify(hobbies));
+
+// val=JSON.parse(localStorage.getItem('hobbies'));
+
+// console.log(val);
+// console.log(localStorage);
+
+
+
+
+//SESSİON STORAGE
+
+// const city =sessionStorage.setItem('city','Mersin');
+// const country=sessionStorage.setItem('country','Türkiye');
+
+// console.log(sessionStorage);
